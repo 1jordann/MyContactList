fa875dbef944a18a597d52ac42668b5903b2f716
1jordann <jordanwilliams788@icloud.com>
2026-02-10 00:35:19 -0500

    im getting starting to get sleepy

--- src/main/java/com/example/mycontactlist/ContactAdapter.java
+++ src/main/java/com/example/mycontactlist/ContactAdapter.java
@@ -1,4 +1,30 @@
 package com.example.mycontactlist;
 
-public class ContactAdapter {
-}
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class ContactAdapter extends RecyclerView.Adapter{
+    private ArrayList<String> contactData;
+    public class ContactViewHolder extends RecyclerView.ViewHolder {
+        public TextView textViewContact;
+
+        public ContactViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textViewContact = itemView.findViewById(R.id.textViewName):
+        }
+
+        public TextView getContactTextView() {
+            return textViewContact;
+        }
+    }
+    public ContactAdapter (ArrayList<String> arraylist){
+        contactData = arraylist;
+    }
+        @NonNull
+        @Override
