b043d460841f813e6378911ece3d8cd26c47b457
1jordann <jordanwilliams788@icloud.com>
2026-02-10 23:38:50 -0500

    Contact Adapter

--- src/main/java/com/example/mycontactlist/ContactListActivity.java
+++ src/main/java/com/example/mycontactlist/ContactListActivity.java
@@ -1,4 +1,38 @@
 package com.example.mycontactlist;
 
-public class ContactListActivity {
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class ContactListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact_list);
+
+        ContactDataSource ds = new ContactDataSource(this);
+        ArrayList<String> names;
+
+        try {
+            ds.open();
+            names = ds.getContactName();
+            ds.close();
+
+            RecyclerView contactList = findViewById(R.id.rvContacts);
+            RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+            contactList.setLayoutManager(layoutManager);
+
+            ContactAdapter contactAdapter = new ContactAdapter(names);
+            contactList.setAdapter(contactAdapter);
+        }
+        catch (Exception e) {
+            Toast.makeText(this, "Error retrieving contacts", Toast.LENGTH_LONG).show();
+        }
+    }
 }
