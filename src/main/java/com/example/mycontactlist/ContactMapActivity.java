f5e3d221af9135792d4735812b5c7a8e7eda0476
1jordann <jordanwilliams788@icloud.com>
2026-02-17 23:15:30 -0500

    Created the layout for the contact map and added the class for the code.

---
+++ src/main/java/com/example/mycontactlist/ContactMapActivity.java
@@ -0,0 +1,13 @@
+package com.example.mycontactlist;
+
+public class ContactMapActivity {
+    private void initGetlocationButton () {
+        Button locationButton = (Button) findViewById(R.id.buttonGetLocation);
+        locationButton. setOnClicklistener (new View. OnClicklistener () {}
+        @Override
+        public void onClick (View view) |
+        EditText editAddress = (EditText) findViewById (R.id.editAddress) ;
+        EditText editCity = (EditText) findViewById(R.id.editCity);
+        EditText editState = (EditText) findViewById (R.id.editState);
+        EditText editZipCode = (EditText) findViewById (R.id.editZipcode);
+}
