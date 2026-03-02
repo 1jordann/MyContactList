8b108d085cf5db87074a2ce258cee25e22b0ee7b
1jordann <jordanwilliams788@icloud.com>
2026-03-02 11:51:00 -0500

    Fixed a spelling error for Battery Level

--- src/main/java/com/example/mycontactlist/ContactListActivity.java
+++ src/main/java/com/example/mycontactlist/ContactListActivity.java
@@ -1,12 +1,16 @@
 package com.example.mycontactlist;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -24,8 +28,21 @@ public class ContactListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_list);
-
         initAddContactButton();
+
+        BroadcastReceiver batteryReceiver = new BroadcastReceiver () {@Override
+        public void onReceive (Context context, Intent intent) {
+            double batteryLevel = intent.getIntExtra (BatteryManager.EXTRA_LEVEL, 0);
+            double levelScale = intent.getIntExtra (BatteryManager.EXTRA_SCALE, 0);
+            int batteryPercent = (int) Math.floor (batteryLevel / levelScale * 100);
+            TextView textBatteryState = (TextView) findViewById(R.id.textBatteryLevel);
+            textBatteryState.setText (batteryPercent + "%");
+        }
+        };
+        IntentFilter filter = new IntentFilter (Intent.ACTION_BATTERY_CHANGED);
+        registerReceiver (batteryReceiver, filter);
+
+
     }
 
     @Override
