c96191bb11e28b4fcc46dc6349b2dc54d64f1ec8
1jordann <jordanwilliams788@icloud.com>
2026-03-05 12:08:21 -0500

    Registering the Sensors

--- src/main/java/com/example/mycontactlist/ContactMapActivity.java
+++ src/main/java/com/example/mycontactlist/ContactMapActivity.java
@@ -6,11 +6,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 
 import android.graphics.Point;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
@@ -23,6 +28,7 @@ import android.view.WindowManager;
 import android.widget.ImageButton;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
@@ -48,6 +54,10 @@ public class ContactMapActivity extends AppCompatActivity implements OnMapReadyC
 
     final int PERMISSION_REQUEST_LOCATION = 101;
     GoogleMap gMap;
+    SensorManager sensorManager;
+    Sensor accelerometer;
+    Sensor magnetometer;
+    TextView textDirection;
     FusedLocationProviderClient fusedLocationClient;
     LocationRequest locationRequest;
     LocationCallback locationCallback;
@@ -63,15 +73,13 @@ public class ContactMapActivity extends AppCompatActivity implements OnMapReadyC
         try {
             ContactDataSource ds = new ContactDataSource(ContactMapActivity.this);
             ds.open();
-            if(extras !=null){
+            if (extras != null) {
                 currentContact = ds.getSpecificContact(extras.getInt("contactid"));
-            }
-            else {
+            } else {
                 contacts = ds.getContacts("contactname", "ASC");
             }
             ds.close();
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             Toast.makeText(this, "Contact(s) could not be retrieved.", Toast.LENGTH_LONG).show();
 
         }
@@ -85,6 +93,19 @@ public class ContactMapActivity extends AppCompatActivity implements OnMapReadyC
 
         createLocationRequest();
         createLocationCallback();
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        magnetometer = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+
+        if (accelerometer != null && magnetometer != null) {
+            sensorManager.registerListener(mySensorEventListener, accelerometer, SensorManager.SENSOR_DELAY_FASTEST);
+            sensorManager.registerListener(mySensorEventListener, magnetometer, SensorManager.SENSOR_DELAY_FASTEST);
+        } else {
+            Toast.makeText(this, "Sensor not found", Toast.LENGTH_LONG).show();
+        }
+        textDirection = (TextView) findViewById(R.id.textHeading);
+
 
     }
 
@@ -108,7 +129,9 @@ public class ContactMapActivity extends AppCompatActivity implements OnMapReadyC
                                     " Accuracy:  " + location.getAccuracy(),
                             Toast.LENGTH_LONG).show();
                 }
-            };
+            }
+
+            ;
         };
     }
 
@@ -116,7 +139,7 @@ public class ContactMapActivity extends AppCompatActivity implements OnMapReadyC
         if (Build.VERSION.SDK_INT >= 23 && ContextCompat.checkSelfPermission(getBaseContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(getBaseContext(), android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             return;
         }
-        fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, null );
+        fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, null);
         gMap.setMyLocationEnabled(true);
     }
 
@@ -136,8 +159,7 @@ public class ContactMapActivity extends AppCompatActivity implements OnMapReadyC
                 RadioButton rbNormal = findViewById(R.id.radioButtonNormal);
                 if (rbNormal.isChecked()) {
                     gMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-                }
-                else  {
+                } else {
                     gMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
                 }
             }
@@ -159,9 +181,9 @@ public class ContactMapActivity extends AppCompatActivity implements OnMapReadyC
         int measuredWidth = size.x;
         int measuredHeight = size.y;
 
-        if (contacts.size()>0) {
+        if (contacts.size() > 0) {
             LatLngBounds.Builder builder = new LatLngBounds.Builder();
-            for (int i=0; i<contacts.size(); i++) {
+            for (int i = 0; i < contacts.size(); i++) {
                 currentContact = contacts.get(i);
 
                 Geocoder geo = new Geocoder(this);
@@ -174,8 +196,7 @@ public class ContactMapActivity extends AppCompatActivity implements OnMapReadyC
 
                 try {
                     addresses = geo.getFromLocationName(address, 1);
-                }
-                catch (IOException e) {
+                } catch (IOException e) {
                     e.printStackTrace();
                 }
                 LatLng point = new LatLng(addresses.get(0).getLatitude(), addresses.get(0).getLongitude());
@@ -184,8 +205,7 @@ public class ContactMapActivity extends AppCompatActivity implements OnMapReadyC
                 gMap.addMarker(new MarkerOptions().position(point).title(currentContact.getContactName()).snippet(address));
             }
             gMap.animateCamera(CameraUpdateFactory.newLatLngBounds(builder.build(), measuredWidth, measuredHeight, 450));
-        }
-        else {
+        } else {
             if (currentContact != null) {
                 Geocoder geo = new Geocoder(this);
                 List<Address> addresses = null;
@@ -197,29 +217,26 @@ public class ContactMapActivity extends AppCompatActivity implements OnMapReadyC
 
                 try {
                     addresses = geo.getFromLocationName(address, 1);
-                }
-                catch (IOException e) {
+                } catch (IOException e) {
                     e.printStackTrace();
                 }
-               // LatLng point = new LatLng(addresses.get(0).getLatitude(),addresses.get(0).getLongitude());
+                // LatLng point = new LatLng(addresses.get(0).getLatitude(),addresses.get(0).getLongitude());
 
-                LatLng point = new LatLng(10,10);
+                LatLng point = new LatLng(10, 10);
 
                 gMap.addMarker(new MarkerOptions().position(point).title(currentContact.getContactName()).snippet(address));
-                gMap.animateCamera(CameraUpdateFactory. newLatLngZoom(point, 16));
+                gMap.animateCamera(CameraUpdateFactory.newLatLngZoom(point, 16));
 
 
-
-
-            }
-            else {
+            } else {
                 AlertDialog alertDialog = new AlertDialog.Builder(ContactMapActivity.this).create();
                 alertDialog.setTitle("No Data");
                 alertDialog.setMessage("No data is available for the mapping function.");
                 alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, "OK", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int which) {
                         finish();
-                    } });
+                    }
+                });
                 alertDialog.show();
             }
         }
@@ -235,22 +252,21 @@ public class ContactMapActivity extends AppCompatActivity implements OnMapReadyC
                                     @Override
                                     public void onClick(View view) {
 
-                                        ActivityCompat.requestPermissions(ContactMapActivity.this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},PERMISSION_REQUEST_LOCATION);
+                                        ActivityCompat.requestPermissions(ContactMapActivity.this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_REQUEST_LOCATION);
                                     }
                                 })
                                 .show();
 
                     } else {
-                        ActivityCompat.requestPermissions(ContactMapActivity.this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},PERMISSION_REQUEST_LOCATION);
+                        ActivityCompat.requestPermissions(ContactMapActivity.this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_REQUEST_LOCATION);
                     }
                 } else {
                     startLocationUpdates();
                 }
-            }else {
+            } else {
                 startLocationUpdates();
             }
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             Toast.makeText(getBaseContext(), "Error requesting permission", Toast.LENGTH_LONG).show();
         }
     }
@@ -261,4 +277,45 @@ public class ContactMapActivity extends AppCompatActivity implements OnMapReadyC
         stopLocationUpdates();
     }
 
+    private SensorEventListener mySensorEventListener = new SensorEventListener() {
+
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        }
+
+        float[] accelerometerValues;
+        float[] magneticValues;
+
+        public void onSensorChanged(SensorEvent event) {
+            if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER)
+                accelerometerValues = event.values;
+            if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD)
+                magneticValues = event.values;
+            if (accelerometerValues != null && magneticValues != null) {
+                float R[] = new float[9];
+                float I[] = new float[9];
+
+                boolean success = SensorManager.getRotationMatrix(R, I, accelerometerValues, magneticValues);
+                if (success) {
+                    float orientation[] = new float[3];
+                    SensorManager.getOrientation(R, orientation);
+
+                    float azimut = (float) Math.toDegrees(orientation[0]);
+                    if (azimut < 0.0f) {
+                        azimut += 360.0f;
+                    }
+                    String direction;
+                    if (azimut >= 315 || azimut < 45) {
+                        direction = "N";
+                    } else if (azimut >= 225 && azimut < 315) {
+                        direction = "W";
+                    } else if (azimut >= 135 && azimut < 225) {
+                        direction = "S";
+                    } else {
+                        direction = "E";
+                    }
+                    textDirection.setText(direction);
+                }
+            }
+        }
+    };
 }
